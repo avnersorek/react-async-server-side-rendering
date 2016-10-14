f3f196ef8e5315cbe7995595b2f9701fe92acb5b
Avner Sorek <avner.sorek@gmail.com>
2016-10-14 19:38:03 +0300

    Type fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Redux SSR Exampe</h1>
+        <h1>Redux Async SSR Example</h1>
         <hr />
         {this.props.children}
       </div>
